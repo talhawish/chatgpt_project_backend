e8bafa027e30b0f9301224c235d1ad50a72c482e
Talha Tahir <talhabhatti0257@gmail.com>
2023-03-02 23:46:59 +0500

    chanegs

--- app/Http/Controllers/Api/ApiRevoChatgptController.php
+++ app/Http/Controllers/Api/ApiRevoChatgptController.php
@@ -112,7 +112,8 @@ class ApiRevoChatgptController extends Controller
         $this->bootstrap($request);
 
         $page = ($request->page ?? 1) + 1;
-        $this->revo->ask('generate a presentation slide number' . $page . 'of maximum 300 words about : ' . $keywords);
+        $totalPages = $request->total_pages ?? 5;
+        $this->revo->ask('generate a presentation slide number' . $page . 'out of ' . $totalPages . ' of maximum 300 words about : ' . $keywords);
 
         $this->response['content'] = property_exists($this->revo->completetion(), 'text') ? $this->revo->completetion()->text : $this->revo->completetion();
 
